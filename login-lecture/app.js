847c9cfad22a649dead038bb9a95256c3f842dbb
soeun823 <bsoeun866@gmail.com>
2024-09-27 00:47:04 +0900

    http 서버

--- login-lecture/app.js
+++ login-lecture/app.js
@@ -1,17 +1,30 @@
 // 서버의 중심 파일 이 될겁니다
 // express 라는 서버 프레임 워크 사용
 
-const express = require("express");
-const app = express();
-
-app.get("/", (req, res) => {
-    res.send("여기는 루트 입니다");
+const http = require("http"); //내장 모듈
+const app = http.createServer( (req, res) => {
+    if(req.url === "/"){
+        res.end("기숙사 가고싶어여");
+    }else if(req.url === "/login"){
+        res.end("여기는 긱사야")
+    }
 });
 
-app.get("/login", (req, res) => {
-    res.send("여기는 로그인 화면 입니다");
+app.listen(3001, () => {
+    console.log("http로 가동된 서버");
 });
 
-app.listen(3000, () => {
-    console.log("서버 가동");
-});
+// const express = require("express");
+// const app = express();
+
+// app.get("/", (req, res) => {
+//     res.send("여기는 루트 입니다");
+// });
+
+// app.get("/login", (req, res) => {
+//     res.send("여기는 로그인 화면 입니다");
+// });
+
+// app.listen(3000, () => {
+//     console.log("서버 가동");
+// });
