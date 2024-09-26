04e6f45cdd971cc3b29c3bbd7b5edd3630f9c3e6
soeun823 <bsoeun866@gmail.com>
2024-09-27 01:25:05 +0900

    하드코딩으로 로그인화면 구현

--- login-lecture/app.js
+++ login-lecture/app.js
@@ -1,31 +1,49 @@
 // 서버의 중심 파일 이 될겁니다
 // express 라는 서버 프레임 워크 사용
 
-const http = require("http"); //내장 모듈
-const app = http.createServer( (req, res) => {
-    res.writeHead(200, {"Content-Type" : "text/html; charset=utf-8"});
-    if(req.url === "/"){
-        res.end("기숙사 가고싶어여");
-    }else if(req.url === "/login"){
-        res.end("여기는 긱사야")
-    }
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send(`
+        <!DOCTYPE html>
+        <html lang="ko">
+
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Document</title>
+        </head>
+
+        <body>
+            여기는 루트입니다.
+        </body>
+
+        </html>
+    `);
 });
 
-app.listen(3001, () => {
-    console.log("http로 가동된 서버");
+app.get("/login", (req, res) => {
+    res.send(`
+        <!DOCTYPE html>
+        <html lang="ko">
+
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Document</title>
+        </head>
+
+        <body>
+            <input type="text" name="" id="" placeholder="아이디"><br>
+            <input type="text" name="" id="" placeholder="비밀번호"><br>
+            <button>로그인</button>
+        </body>
+
+        </html>
+    `);
 });
 
-// const express = require("express");
-// const app = express();
-
-// app.get("/", (req, res) => {
-//     res.send("여기는 루트 입니다");
-// });
-
-// app.get("/login", (req, res) => {
-//     res.send("여기는 로그인 화면 입니다");
-// });
-
-// app.listen(3000, () => {
-//     console.log("서버 가동");
-// });
+app.listen(3000, () => {
+    console.log("서버 가동");
+});
