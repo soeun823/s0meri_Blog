f8ba6433f384aa5428b056a6b70faa0dea3c0972
soeun823 <bsoeun866@gmail.com>
2024-09-27 00:19:56 +0900

    express로 서버 띄워보기

--- login-lecture/app.js
+++ login-lecture/app.js
@@ -4,6 +4,14 @@
 const express = require("express");
 const app = express();
 
+app.get("/", (req, res) => {
+    res.send("여기는 루트 입니다");
+});
+
+app.get("/login", (req, res) => {
+    res.send("여기는 로그인 화면 입니다");
+});
+
 app.listen(3000, () => {
     console.log("서버 가동");
 });
