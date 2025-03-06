ed98a66f5541193938c667755cb2724bdcb0e2f2
soeun823 <bsoeun866@gmail.com>
2025-03-06 23:57:24 +0900

    feat :: 벡엔드에서 로그인 구현

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -9,9 +9,30 @@ const output = {
         res.render("home/login");
     },
 };
+
+const users={
+    id : ["안민주","문소정","소메리"],
+    psword : ["1234","1234","2345"],
+}
+
 const process ={
     login: (req,res)=>{
-        console.log(req.body);
+        const id = req.body.id,
+        psword = req.body.psword;
+        console.log(id,psword);
+
+        if (users.id.includes(id)){
+            const idx = users.id.indexOf(id);
+            if (users.psword[idx] === psword){
+                return res.json({
+                    success: true,
+                });
+            }
+        }
+        return res.json({
+            success: false,
+            msg:"로그인에 실패하셨습니다.",
+        });
     },
 }
 
