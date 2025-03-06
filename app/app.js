630930e60f307a8a4dfbe384e424629f3eb9cce2
soeun823 <bsoeun866@gmail.com>
2025-03-06 20:18:47 +0900

    docs :: 로그인 js 추가

--- app/app.js
+++ app/app.js
@@ -9,10 +9,12 @@ const app = express();
 
 //라우팅
 const home = require("./src/routes/home");//상대적으로 명시
+const { dirname } = require("path");
 
 //앱 세팅
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
+app.use(express.static(`${__dirname}/src/public`));
 
 app.use("/",home); //use() -> 미들웨어를 등록 해주는 매서드..
 
