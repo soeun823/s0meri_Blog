11543d8cadf0fc95fcb8b8f37a84c0771034dab2
soeun823 <bsoeun866@gmail.com>
2025-03-09 00:46:20 +0900

    test :: 데이터 파일로 관리

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -1,14 +1,9 @@
 "use strict"
 
+const fs = require("fs");
 class UserStorage{
-	static #users={ //#표시는 private접근 권한
-    id : ["nono","moon","s0meri"],
-    psword : ["1234","1234","2345"],
-		name: ["안민주","문소정","소메리"]
-	};
-
 	static getUsers(...fields){
-		const users = this.#users;
+		// const users = this.#users;
 		const newUsers = fields.reduce((newUsers, field) => {
 			if(users.hasOwnProperty(field)){
 				newUsers[field] = users[field];
@@ -18,18 +13,22 @@ class UserStorage{
 		return newUsers;
 	}
 	static getUserInfo(id){
-		const users = this.#users;
-		const idx = users.id.indexOf(id);
-		const usersKeys= Object.keys(users);
-		const userInfo = usersKeys.reduce((newUsers, info)=>{
-			newUsers[info] = users[info][idx];
-			return newUsers;
-		},{});
-		return userInfo;
+		fs.readFile('./src/databases/users.json',(err,data) => {
+			if (err) throw err;
+			const users = JSON.parse(data);
+			const idx = users.id.indexOf(id);
+			const usersKeys= Object.keys(users);
+			const userInfo = usersKeys.reduce((newUsers, info)=>{
+				newUsers[info] = users[info][idx];
+				return newUsers;
+			},{});
+			return userInfo;
+		})
+
 	}
 
 	static save(userInfo){
-		const users = this.#users;
+		// const users = this.#users;
 		users.id.push(userInfo.id);
 		users.name.push(userInfo.name);
 		users.psword.push(userInfo.psword);
